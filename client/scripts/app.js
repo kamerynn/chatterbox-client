cbcec6f495e78d9e55ae8f70a7092ac278be5b9a
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-13 18:48:21 -0400

    Implement fetch function that requests and displays messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,7 @@
 // YOUR CODE HERE:
 
 var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
   init: function() {
 
   },
@@ -8,6 +9,32 @@ var app = {
 
   },
   fetch: function() {
-    
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      //data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        // Loop over results array
+        _.each(data.results, function(message) {
+          // Create a node for each object
+          var container = $("<div class='message'></div>");
+          var user = $("<span></span>").text(message.username);
+          var text = $("<p></p>").text(message.text);
+
+          container.append(user);
+          container.append(text);
+
+          // Append the node to the #main element
+          $("#main").append(container);
+        })
+        console.log(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
   }
 }
